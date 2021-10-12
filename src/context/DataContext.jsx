1db81127eb3a6380436bf85a514dc6df05b229ba
Tony Velazquez <tonymvg24@gmail.com>
2021-10-12 18:37:13 -0500

    testing initial state

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useReducer } from 'react';
-import { storeReducer } from '../reducer/storeReducer';
+import { storeReducer, storeInitialState } from '../reducer/storeReducer';
 
 export const DataContext = createContext();
 
@@ -7,14 +7,14 @@ export const DataContext = createContext();
 // 	return JSON.parse(localStorage.getItem('state')) || [];
 // };
 
-const storeLocalStorage = JSON.parse(localStorage.getItem('state') || []);
+// const storeLocalStorage = JSON.parse(localStorage.getItem('state') || []);
 
 export const DataProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(storeReducer, storeLocalStorage);
+	const [state, dispatch] = useReducer(storeReducer, storeInitialState);
 
-	useEffect(() => {
-		localStorage.setItem('state', JSON.stringify(state));
-	}, [state]);
+	// useEffect(() => {
+	// 	localStorage.setItem('state', JSON.stringify(state));
+	// }, [state]);
 
 	return (
 		<DataContext.Provider value={{ state, dispatch }}>

43016f1c560afff2344de988c178db30bd12e440
Tony Velazquez <tonymvg24@gmail.com>
2021-10-12 19:00:00 -0500

    remove missing variable

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,10 +1,20 @@
-import React, { createContext, useEffect, useReducer } from 'react';
+import React, { createContext, useReducer } from 'react';
 import { storeReducer, storeInitialState } from '../reducer/storeReducer';
 
 export const DataContext = createContext();
 
+// const init = () => {
+// 	return JSON.parse(localStorage.getItem('state')) || [];
+// };
+
+// const storeLocalStorage = JSON.parse(localStorage.getItem('state') || []);
+
 export const DataProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(storeReducer, storeInitialState);
+
+	// useEffect(() => {
+	// 	localStorage.setItem('state', JSON.stringify(state));
+	// }, [state]);
 
 	return (
 		<DataContext.Provider value={{ state, dispatch }}>

c8da804fcb1d3b11e726691b8c3dedb8f35779c5
Tony Velazquez <tonymvg24@gmail.com>
2021-10-15 19:38:08 -0500

    local storage has been added

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,10 +1,23 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { storeInitialState, storeReducer } from '../reducer/storeReducer';
 
 export const DataContext = createContext();
 
+const init = () => {
+	const storeState = JSON.parse(localStorage.getItem('state'));
+	if (storeState) {
+		return JSON.parse(localStorage.getItem('state'));
+	} else {
+		return localStorage.setItem('state', JSON.stringify(storeInitialState));
+	}
+};
+
 export const DataProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(storeReducer, storeInitialState);
+	const [state, dispatch] = useReducer(storeReducer, storeInitialState, init);
+
+	useEffect(() => {
+		localStorage.setItem('state', JSON.stringify(state));
+	}, [state]);
 
 	return (
 		<DataContext.Provider value={{ state, dispatch }}>
